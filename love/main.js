486f25b164e9ef5eb126fc78bc44153e3ea937bc
All_yours <ashishkumarpandit111111@gmail.com>
2025-06-30 13:35:48 +0530

    main.js

--- love/main.js
+++ love/main.js
@@ -141,7 +141,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Initialize the GL context
-var gl = canvas.getContext('webgl');
+//var gl = canvas.getContext('webgl');
 if(!gl){
   console.error("Unable to initialize WebGL.");
 }
